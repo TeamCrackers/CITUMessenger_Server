85b8031b4a736a04764bd1d5346c5e555d96065b
Ted Ian Osias <Ted Ian Osias>
2015-12-16 09:17:27 +0800

    Added Connect and Disconnection Event in Chat Server

--- ChatServer.js
+++ ChatServer.js
@@ -1,4 +1,4 @@
-function initializeChatServer(http){
+exports.initializeChatServer = function(http){
 	// EVENTS
 	var NEW_MESSAGE = "NEW MESSAGE";
 
@@ -11,7 +11,20 @@ function initializeChatServer(http){
 
 	server.io = require('socket.io')(http);
 	server.io.on('connection', function(socket){
-	  
+		socket.on('userid',function(data){
+			server.users[socket] = data;
+			console.log(server.users);
+			socket.emit("online users update",server.users);
+		});
+		socket.on("disconnect",function(){
+			var index = server.users.indexOf(socket);
+			var user = server.users[socket];
+			if(index>-1){
+				server.users.splice(index,1);
+			}
+			console.log(user+" has disconnected");
+		});
+
 	});
 	return server;
 }
--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 // GLOBALS
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 var mongoose = require('mongoose');
+
+
+
+app.use(express.static('public'));
+
 mongoose.connect('mongodb://localhost/test');
 
+
+
 var userController = require("./Controllers/UserController.js").UserController(jsonParser,mongoose);
+
+
 app.use('/user',userController);   // Use UserController
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -15,3 +25,6 @@ app.get('/', function(req, res){
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+
+var server =  require("./ChatServer.js").initializeChatServer(http);
