bcb040154a647ff99436f7b36d211d8eea132c89
Ted Ian Osias <osiastedian@gmail.com>
2015-12-30 16:08:12 +0800

    Can now store into memory online users

--- ChatServer.js
+++ ChatServer.js
@@ -4,25 +4,31 @@ exports.initializeChatServer = function(http){
 
 	var server = {
 		io: null,
-		users:[],
+		users: [],
 		rooms:[],
 		
 	};
 
 	server.io = require('socket.io')(http);
-	server.io.on('connection', function(socket){
-		socket.on('userid',function(data){
-			server.users[socket] = data;
+	server.io.on('connection', function(client){
+		var object = {
+				user : {},
+				socket: {}
+			};
+		client.on('userid',function(data){
+			object.user = data;
+			object.socket = client;
+			server.users.push(object);
+			console.log(server.users.length);
 			console.log(server.users);
-			socket.emit("online users update",server.users);
 		});
-		socket.on("disconnect",function(){
-			var index = server.users.indexOf(socket);
-			var user = server.users[socket];
+		client.on("disconnect",function(){
+			var index = server.users.indexOf(object);
 			if(index>-1){
 				server.users.splice(index,1);
 			}
-			console.log(user+" has disconnected");
+			console.log(object.user+" has disconnected");
+			console.log(server.users.length);
 		});
 
 	});
