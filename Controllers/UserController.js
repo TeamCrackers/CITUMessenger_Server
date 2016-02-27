599daa8bdc0d47ee7c5f23f88989b68095c8c1be
Ted Ian Osias <osiastedian@gmail.com>
2016-02-28 06:06:45 +0800

    Added AddFriend and IsFriend Functionality

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -8,14 +8,21 @@ exports.UserController = function(jsonParser,mongoose){
 		service.login(req.body.schoolId,req.body.password,returnFunction);
 	});
 	UserController.post('/register',jsonParser,function(req,res){
-		service.register(
+		res.json(service.register(
 						req.body.schoolId,
 						req.body.firstName,
 						req.body.lastName,
 						req.body.course,
 						req.body.nickName,
-						req.body.password);
-		res.json(true);
-	});
+						req.body.password)
+		);
+    });
+    UserController.post('/addFriend',jsonParser,function (request,response) {
+        service.addFriend(request.body.user1,request.body.user2,response)
+    });
+    UserController.post('/isFriend',jsonParser,function (request,response) {
+        service.isFriend(request.body.user1,request.body.user2,response)
+    });
+	
 	return UserController;
 }
--- Service/UserService.js
+++ Service/UserService.js
@@ -39,7 +39,7 @@ exports.UserService = function(mongoose){
 			
 		},
 		register : function(schoolId,firstname,lastname,course,nickname,password){
-			var user = new this.UserModel();
+            var user = new this.UserModel();
 			user.schoolId = schoolId;
 			user.firstName = firstname;
 			user.lastName = lastname;
@@ -49,9 +49,44 @@ exports.UserService = function(mongoose){
 			user.save(function(err){
 				if(err){
 					console.log("Register Error");
+                    return false;
 				}
 			});
-		}
+            return true;
+		},
+        addFriend: function(user1SchooldId,user2SchoolId,response){
+            var userModel = this.UserModel;
+            var query1 = userModel.findOne({"schoolId":user1SchooldId});
+            var query2 = userModel.findOne({"schoolId":user2SchoolId});
+            query1.exec(function(err,user1){
+                if(!err)
+                    query2.exec(function(err,user2){
+                        var user = user1;
+                        user1.friends.push(user2.schoolId);
+                        user2.friends.push(user1.schoolId);
+                        user1.save();
+                        user2.save();
+                        response.json(true);
+                    })
+                else
+                    response.json(false);
+            });
+        },
+        isFriend: function(user1SchooldId,user2SchoolId,response){
+            var userModel = this.UserModel;
+            var query = userModel.findOne({"schoolId":user1SchooldId});
+            query.exec(function(err,user){
+                var found = false;
+                if(user!=null)
+                    for(var i=0;i<user.friends.length;i++)
+                        if(user.friends == user2SchoolId)
+                            {
+                                found = true;
+                                break;
+                            }
+                response.json(found);
+            });
+        }
 	};
 	return service;
 }
